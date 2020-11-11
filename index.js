2cc905f4e2e9d6f41aa1a7750eba58f6e94662ed
Rabiul Alam <rabiul.alam@outlook.com>
2020-11-10 22:40:07 -0500

    done

--- index.js
+++ index.js
@@ -1,78 +1,73 @@
-console.log("Welcome to the backend :) ")
-const express = require('express');
+console.log("Welcome to the backend :) ");
+const express = require("express");
 const app = express();
-const path = require('path');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const path = require("path");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log('Server listening at port %d', port);
+  console.log("Server listening at port %d", port);
 });
 
-//All teh players on teh board 
-let squares = []
+//All the players on the board
+let squares = [];
 
-io.on('connection', function (socket) {
+io.on("connection", function (socket) {
+  console.log(`user ${socket.id} has connected`);
+  socket.emit("yourId", { id: socket.id });
 
-    console.log(`user ${socket.id} has connected`)
-    socket.emit('yourId', { id: socket.id })
+  let square = {
+    x: Math.random() * 500,
+    y: Math.random() * 500,
+    w: 50,
+    h: 50,
+    color: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+    id: socket.id,
+  };
+  squares.push(square);
+  console.log(io.sockets);
+  io.sockets.emit("sendSquaresFromServer", { squares });
 
-    let square = {
-        x: Math.random() * 500,
-        y: Math.random() * 500,
-        w: 50,
-        h: 50,
-        color: "#" + ((1 << 24) * Math.random() | 0).toString(16),
-        id: socket.id
+  //socket.emit('keyFromClient', { key: event.key, id: yourId })
+  socket.on("keyFromClient", function (data) {
+    // console.log(data)
+    let square = squares.find((eachSq) => {
+      return eachSq.id === data.id;
+    });
+    if (square) {
+      switch (data.key) {
+        case "ArrowLeft":
+          square.x -= 10;
+
+          break;
+        case "ArrowRight":
+          square.x += 10;
+
+          break;
+        case "ArrowUp":
+          square.y -= 10;
+
+          break;
+        case "ArrowDown":
+          square.y += 10;
+
+          break;
+      }
+      io.sockets.emit("sendSquaresFromServer", { squares });
     }
-    squares.push(square)
-    io.sockets.emit('sendSquaresFromServer', { squares })
+  });
 
-    //socket.emit('keyFromClient', { key: event.key, id: yourId })
-    socket.on('keyFromClient', function (data) {
-        console.log(data)
-        let square = squares.find(eachSq => {
-            return eachSq.id === data.id
-        })
-        if (square) {
-            switch (data.key) {
-                case 'ArrowLeft':
-                    square.x -= 10
-
-                    break;
-                case 'ArrowRight':
-                    square.x += 10
-
-                    break;
-                case 'ArrowUp':
-                    square.y -= 10
-
-                    break;
-                case 'ArrowDown':
-                    square.y += 10
-
-                    break;
-            }
-            io.sockets.emit('sendSquaresFromServer', { squares })
-        }
-    })
-
-
-    socket.on('disconnect', function (socket) {
-
-        console.log(`${this.id} has disconnected`)
-        let id = this.id
-        console.log(squares)
-        squares = squares.filter(eachSq => {
-            return eachSq.id !== id
-        })
-        io.sockets.emit('sendSquaresFromServer', { squares })
-    })
-
-})
-
-
+  socket.on("disconnect", function (socket) {
+    console.log(`${this.id} has disconnected`);
+    let id = this.id;
+    console.log(squares);
+    squares = squares.filter((eachSq) => {
+      return eachSq.id !== id;
+    });
+    io.sockets.emit("sendSquaresFromServer", { squares });
+  });
+});
 
 // Routing
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
